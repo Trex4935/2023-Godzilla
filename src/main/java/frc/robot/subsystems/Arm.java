642aac01558429ceadfcf87852bda930d4805934
Yoofi <yoofiae@gmail.com>
2023-01-14 09:06:57 -0500

    Removed limit switches from Arm subsystem

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -6,7 +6,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DigitalOutput;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -21,7 +20,6 @@ public class Arm extends SubsystemBase {
   CANSparkMax ArmRotation;
   WPI_TalonFX ArmExtendingMotor;
   WPI_TalonFX AEM;
-  Encoder encoder;
   FlippedDIO limitSwitchBottom;
   FlippedDIO limitSwitchTop;
 
@@ -32,57 +30,25 @@ public class Arm extends SubsystemBase {
     //Arm Extension
     ArmExtendingMotor = new WPI_TalonFX(TalonFXID);
     AEM = ArmExtendingMotor;
-
-
   }
   // note for me: gear ratio is 200:1; incorporate later
 public void moveArm(){
   
 }
-// checks bottom limits and return
 
-public boolean stopBottomLimits(){
-  return limitSwitchBottom.get() || encoder.get() <= 4000;
-}
-
-public boolean extendToBottom(XboxController ctrl){
-  extendArm(ctrl);
-  stopBottomLimit();
-  stopEncoderBottomLimit();
-  return stopBottomLimits();
-}
-
-// If limit switch is triggered, then stop the motor from extending 
-public void stopBottomLimit(){
-
-  if (limitSwitchBottom.get()) {
-    AEM.stopMotor();
-  };
-
-
-
-}
-
-// If Encoder is negative then stop the motor from extending 
-public void stopEncoderBottomLimit(){
-  if (encoder.get() <= 4000){
-    AEM.stopMotor();
-  }
-}
-
-
-// move the motor depending on my joystick inputs
 public void extendArm(XboxController xboxController) {
-  AEM.set(xboxController.getRawAxis(1));
- 
- }
+  if (limitSwitchTop.get() == false)
+ AEM.set(xboxController.getRawAxis(1));
+ else {AEM.stopMotor();}
 
+ if (limitSwitchBottom.get() == false)
+ AEM.set(xboxController.getRawAxis(0));
+ else {AEM.stopMotor();}
+}
 
- 
- 
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduled run
+    // This method will be called once per scheduler run
   }
 }

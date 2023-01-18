4f82d52f6a6faf37d2c399f9fb9b3f32b7c0eea0
HamsterRat <ellenpham08@gmail.com>
2023-01-18 17:57:33 -0500

    added additional condition of controller axis

--- src/main/java/frc/robot/subsystems/ArmRotation.java
+++ src/main/java/frc/robot/subsystems/ArmRotation.java
@@ -18,6 +18,8 @@ import edu.wpi.first.wpilibj.DigitalOutput;
 
 public class ArmRotation extends SubsystemBase {
   CANSparkMax ArmRotation;
+  XboxController xboxController;
+
   //init a DigitalInput on channels 0 and 1; top is forward and bottom is backward
   DigitalInput toplimitSwitch = new DigitalInput(0);
   DigitalInput bottomlimitSwitch = new DigitalInput(1);
@@ -26,39 +28,35 @@ public class ArmRotation extends SubsystemBase {
   public ArmRotation() {
     // init motor
     ArmRotation = SparkMax.createDefaultCANSparkMax(ArmRotationConstants.armRotationCAN);
-   //init a DigitalInput on channels 0 and 1; top is forwards and bottom is backwards
+   
   
 
-    // sets the speed that the arm moves forward ()
+    // sets the speed that the arm moves forward (now declares the limit switch)
   }
 
   public void moveArmForward() {
-    if (toplimitSwitch.get() == true) {
+    if (toplimitSwitch.get() == true && xboxController.getRawAxis(4) >= 0) {
       // if the toplimitSwitch is true, stop the motors
-      if (toplimitSwitch.get()){
-       ArmRotation.set(.01);
-      }else{
+       ArmRotation.stopMotor();
+    } else {
         // if the toplimitSwitch is false, then allow motor to keep moving
         ArmRotation.set(1);
       }
     }
     
-  }
+  
 
-  // sets the speed that the arm moves backward
+  // sets the speed that the arm moves backward (now declares the limit switch)
   public void moveArmBackward() {
-    ArmRotation.set(-1);
-    if ( bottomlimitSwitch.get() == true) {
+    if ( bottomlimitSwitch.get() == true && xboxController.getRawAxis(4) <= 0) {
       // if the bottomlimitSwitch is true,stop the motor 
-      if(bottomlimitSwitch.get()){
-        ArmRotation.set(.01);
-      
-      }else{
+      ArmRotation.stopMotor();
+    } else {
         //if the bottomlimitSwitch is false, then allow the motor to keep moving
         ArmRotation.set(1);
       }
     }
-  }
+  
 
   // stops the ArmRotation motor
   public void stopArmRotation() {

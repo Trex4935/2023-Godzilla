4565b3901603698571acc916e2e0a8e19e79dff8
Popqwas <sawqpop@gmail.com>
2023-01-26 16:34:24 -0500

    removed drivetrain constant class

--- src/main/java/frc/robot/commands/ca_autoTurnKinematic.java
+++ src/main/java/frc/robot/commands/ca_autoTurnKinematic.java
@@ -7,7 +7,6 @@ package frc.robot.commands;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.Constants.MovementConstraints;
 import frc.robot.subsystems.Drivetrain;
 

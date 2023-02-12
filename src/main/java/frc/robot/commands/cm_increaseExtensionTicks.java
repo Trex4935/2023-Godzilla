dd9e3ae71207718a9a8948fbc9e329ad937d4ced
Canthv0 <matbyrd@forthebyrds.com>
2023-02-12 14:13:24 -0500

    added end command for manual

--- src/main/java/frc/robot/commands/cm_increaseExtensionTicks.java
+++ src/main/java/frc/robot/commands/cm_increaseExtensionTicks.java
@@ -33,6 +33,6 @@ public class cm_increaseExtensionTicks extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
